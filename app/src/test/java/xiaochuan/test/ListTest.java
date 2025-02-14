30fc69fd612c5f360f7034f40b1d8d99291da813
freexiaochuan <w2239559319@myyahoo.com>
2025-02-14 14:46:08 +0800

    add to list

--- app/src/test/java/xiaochuan/test/ListTest.java
+++ app/src/test/java/xiaochuan/test/ListTest.java
@@ -3,6 +3,7 @@ package xiaochuan.test;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -18,5 +19,28 @@ public class ListTest {
 
         assertEquals(l1, l2);
         assertArrayEquals(a1, a2);
+    }
+
+    /**
+     * object
+     */
+    @Test
+    public void t2() {
+        String[] a1 = new String[]{ "apple", "orange" };
+
+        List<String> l1 = Arrays.asList(a1);
+        
+        String[] a2 = l1.toArray(new String[0]);
+
+        assertArrayEquals(a1, a2);
+
+        assertThrows(UnsupportedOperationException.class, () -> l1.add("hello"));
+
+
+        List<String> l2 = new ArrayList<>(Arrays.asList(a1));
+        
+        l2.add("hello");
+        assertEquals(l2.size(), 3);
+
     }
 }
